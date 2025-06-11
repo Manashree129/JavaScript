b808fe951e08ed262e734af71ac8f504e7ce76ab
Manashree <149083963+Manashree129@users.noreply.github.com>
2025-06-11 12:21:30 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+console.log(5==5);
+console.log(5>4);
+console.log(4<3);
+console.log(3>=2);
+console.log(3<=2);
+console.log(2!=3);
+
+console.log("--------------------------")
+console.log("2" > 1);
+console.log("02">1);
+
+console.log("---------------------------")
+console.log(null == 0);// does not converts null to 0
+console.log(null > 0);// converts null to 0
+console.log(null >= 0);// converts null to 0
+
+console.log(undefined >= 0); 
+//undefined never convert to 0
