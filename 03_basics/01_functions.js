9241b2b00e1f50be150194bfc9d1042ad1aff3e4
Manashree <149083963+Manashree129@users.noreply.github.com>
2025-06-19 18:37:36 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,14 @@
+function sayMyName(){
+    console.log("M");
+    console.log("A");
+    console.log("N");
+    console.log("A");
+    console.log("S");
+    console.log("H");
+    console.log("R");
+    console.log("E");
+    console.log("E");   
+}
+
+sayMyName()
+
