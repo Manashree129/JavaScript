05ecf226ace08c5f0e23640cf386bcc216607bcf
Manashree <149083963+Manashree129@users.noreply.github.com>
2025-06-24 07:36:04 +0000

    arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -13,8 +13,63 @@ const user = {
 
 // user.username ="sam"
 // user.welcomeMessage()
-console.log(this)
-    }
+
+console.log(this) // in node environment "this" refers to empty object
+
+// in browser global object is window object
+
+function chai(){
+    let username = "manashree"
+    console.log(this); //we cant use this in function (doesnt work) [if only "this" is used it refers to global object]
+}
+chai()
+
+
+// const chai = function (){
+//     let username = "manashree"
+//     console.log(this.username); //we cant use this in function (doesnt work)
+// }
+// chai()
+
+
+
+
+console.log("+++++++++++++++++++++++++++++++++++++++++")
+
+const one = () => {
+    let username = "manashree"
+    console.log(this); 
+}
+chai()
+
+const addTwo = (num1, num2) => {
+    return num1 + num2 
+
+}
+
+console.log(addTwo(3, 4))
+
+const subTwo = (num1, num2) =>num1 - num2 // implicit return (it assumes return we dont need to write "return")
+
+console.log(subTwo(3, 4))
+
+const mulTwo = (num1, num2) =>(num1 * num2) //we can wrap it in parenthesis (if wrapped in curly braces return keyword is must)
+
+const name = () => ({username: "hitesh"})
+
+console.log(name())
+
+// const myArray = [2, 3 ,5]
+
+// myArray.forEach(() => ())
+
+
+
+
+
+
+
+   
 
 
 
